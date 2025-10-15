24a0b25fa50807c4c1ebaaf6778512d3012e1288
Mark Yuan <mark@webasone.com>
2025-10-15 19:19:41 +0000

    a

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <!-- use vue3 and bootstrap 4 to create a blogpost. The post data is read from a json file. The main page display the title, data, author, image, summary and a read more link to read the content
   -->
 
-  <div v-if="city" class="d-flex justify-content-center p-3 bg-light">
+  <div v-if="city && state" class="d-flex justify-content-center p-3 bg-light">
     <RouterLink active-class="bg-success bg-opacity-50 border-danger rounded" class="px-3 m-1 border  text-primary"
       to="/">Local listing
     </RouterLink>
@@ -13,6 +13,9 @@
     <RouterLink active-class="bg-success bg-opacity-50 border-danger rounded" class="px-3 m-1 border   text-primary"
       to="/FindStateCity">Other's
       listing
+    </RouterLink>
+    <RouterLink active-class="bg-success bg-opacity-50 border-danger rounded" class="px-3 m-1 border   text-primary"
+      to="/FAQ">FAQ
     </RouterLink>
   </div>
   <div v-else>
@@ -26,8 +29,12 @@ import { ref } from 'vue';
 import FindStateCity from './components/FindStateCity.vue';
 
 const city = ref('');
+const state = ref('');
 if ('asoneCity' in window) {
   city.value = window.asoneCity;
+}
+if ('asoneState' in window) {
+  state.value = window.asoneState;
 }
 
 
--- src/router.ts
+++ src/router.ts
@@ -2,22 +2,15 @@ import { createRouter, createWebHistory } from 'vue-router';
 import States from './components/States.vue';
 import ShowList from './components/ShowList.vue';
 import FindStateCity from './components/FindStateCity.vue';
+import FAQ from './components/FAQ.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    {
-      path: '/Request',
-      component: States
-    },
-    {
-      path: '/',
-      component: ShowList
-    },
-    {
-      path:'/FindStateCity',
-      component: FindStateCity
-    }
+    { path: '/Request', component: States},
+    { path: '/', component: ShowList},
+    { path:'/FAQ', component: FAQ},
+    { path:'/FindStateCity',component: FindStateCity }
   ],
   scrollBehavior() {
     return { top: 0 }; // Always scroll to top
