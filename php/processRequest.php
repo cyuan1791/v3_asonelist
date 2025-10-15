4d2b63bcb5f0dcdf0fa51e0f337020dcc2591fbc
Mark Yuan <mark@webasone.com>
2025-10-15 18:10:45 +0000

    a

--- php/processRequest.php
+++ php/processRequest.php
@@ -1,4 +1,5 @@
 <?php
+      // change this need to update v3_request:php/processRequest.php
       include 'ashare.php';
 
       if (json_last_error() !== JSON_ERROR_NONE) {
@@ -9,27 +10,33 @@
       }
        $json_data = file_get_contents("php://input");
        $data_array = json_decode($json_data, true); // Decodes to an associative array
-       $base= get_docroot()."../../www.asonelist.com/list";
+        $base= get_docroot()."../../www.asonelist.com/list";
        if (!is_dir($base) ) mkdir ($base);
        $jsonFilePath = $base.'/lastestsubmit.json';
-       if (file_exists($jsonFile)) {
-           $jsonContent = file_get_contents($jsonFilePath);
+       $lockFile = $base.'/lastestsubmit.lock';
+       $handle = fopen($lockFile, "w");
+       if (flock($handle, LOCK_EX)) {
+         if (file_exists($jsonFilePath)) {
+             $jsonContent = file_get_contents($jsonFilePath);
 
-           // Decode the JSON string into a PHP array or object
-           $data = json_decode($jsonContent, true); // true for associative array, false for object
+             // Decode the JSON string into a PHP array or object
+             $data = json_decode($jsonContent, true); // true for associative array, false for object
 
-           if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
+             if ($data === null && json_last_error() !== JSON_ERROR_NONE) {
               // Handle JSON decoding errors
-              echo "Error decoding JSON: " . json_last_error_msg();
-           }
-        } else {
-          $data = [];
-        }
-        $data_array['status'] = 0;
-        $data_array['RequestTime'] = time();
+                echo "Error decoding JSON: " . json_last_error_msg();
+             }
+          } else {
+            $data = [];
+          }
+          $data_array['status'] = 0;
+          $data_array['RequestTime'] = time();
 
-        $data[] = $data_array; 
-        file_put_contents($jsonFilePath,json_encode($data, JSON_PRETTY_PRINT));  
+          $data[] = $data_array; 
+          file_put_contents($jsonFilePath,json_encode($data, JSON_PRETTY_PRINT));  
+          flock($handle, LOCK_UN);
+      }
+      fclose($handle);
       
       header('Content-Type: application/json');
       echo json_encode([ 'msg' => 'append to '.$jsonFilePath]);
